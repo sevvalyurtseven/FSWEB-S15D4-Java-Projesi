55e304306a4afa4b27b17c4d5f7d45e43fb41bbf
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-02-05 17:35:58 +0300

    stack challenge tamamlandi

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,6 +12,49 @@ public class Main {
     }
 
     public static boolean checkForPalindrome(String text) {
+        System.out.println("Initial text = " + text);
+        text = text.replaceAll("[.,'?\\-\\s]", "");
+        Stack<Character> stackOriginal = new Stack<>();
+        var stackTemp = new Stack<>(); //normal stack
+        var stackReversed = new Stack<>(); // ters cevrilmis stack
+
+        var chars = text.toCharArray();
+        for(var c : chars){
+            stackOriginal.push(c);
+            /*
+              l
+              e
+              h
+             */
+            stackTemp.push(c);
+            /*
+              l
+              e
+              h
+             */
+        }
+
+        for(char c : chars){
+            Object charTemp = stackTemp.pop();
+            stackReversed.push((Character)charTemp);
+            /*
+              l
+              e
+              h
+             */
+
+            /* stackReversed
+            h
+            e
+            l
+             */
+        }
+        System.out.println("Reversed = " + stackReversed);
+        System.out.println("Original = " + stackOriginal);
+
+        if(stackOriginal.equals(stackReversed)){
+            return true;
+        }
         return false;
     }
 }
